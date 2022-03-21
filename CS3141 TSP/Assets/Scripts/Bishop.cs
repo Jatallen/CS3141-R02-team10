71442da7ad1c0907e1e8ebb2a7c185fb94906818
jkyt07 <71299182+jkyt07@users.noreply.github.com>
2022-03-21 10:59:26 -0400

    Bishop + Queen Moves
    Bishop and Queen can now move to their appropriate locations

---
+++ CS3141 TSP/Assets/Scripts/Bishop.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bishop : Piece
+{
+    public override List<Vector2> ListMoves()    //returns an array of the relative locations the piece can move
+    {
+        List<Vector2> moves = new List<Vector2>();
+        float tile = GameController.tile;
+        //Bishop moves
+        int j = 1;
+        while (position.x + j * tile < 4 && position.y + j * tile < 4 && !PointCollidesWithTeam(new Vector2(tile * j, tile * j) + position, gameObject.tag))
+        {
+            moves.Add(new Vector2(tile * j, tile * j));    //top right
+            if (PointCollidesWithTeam(new Vector2(tile * j, tile * j) + position, OtherTeam(gameObject.tag)))
+                break;
+            j++;
+        }
+        j = 1;
+        while (position.x + j * tile < 4 && position.y - j * tile > -4 && !PointCollidesWithTeam(new Vector2(tile * j, tile * j * -1) + position, gameObject.tag))
+        {
+            moves.Add(new Vector2(tile * j, tile * j * -1));    //bottom right
+            if (PointCollidesWithTeam(new Vector2(tile * j, tile * j * -1) + position, OtherTeam(gameObject.tag)))
+                break;
+            j++;
+        }
+        j = 1;
+        while (position.x - j * tile > -4 && position.y - j * tile > -4 && !PointCollidesWithTeam(new Vector2(tile * j * -1, tile * j * -1) + position, gameObject.tag))
+        {
+            moves.Add(new Vector2(tile * j * -1, tile * j * -1));    //bottom right
+            if (PointCollidesWithTeam(new Vector2(tile * j * -1, tile * j * -1) + position, OtherTeam(gameObject.tag)))
+                break;
+            j++;
+        }
+        j = 1;
+        while (position.x - j * tile > -4 && position.y + j * tile < 4 && !PointCollidesWithTeam(new Vector2(tile * j * -1, tile * j) + position, gameObject.tag))
+        {
+            moves.Add(new Vector2(tile * j * -1, tile * j));    //top right
+            if (PointCollidesWithTeam(new Vector2(tile * j * -1, tile * j) + position, OtherTeam(gameObject.tag)))
+                break;
+            j++;
+        }
+
+        for (int i = moves.Count - 1; i >= 0; i--)
+        {
+            moves[i] += position;
+            if (!GameController.boardColl.OverlapPoint(moves[i]) ||
+                PointCollidesWithTeam(moves[i], gameObject.tag))  //remove move if it's an illegal space
+                moves.Remove(moves[i]);
+        }
+
+        return moves;
+    }
+}
--- CS3141 TSP/Assets/Scripts/Queen.cs
+++ CS3141 TSP/Assets/Scripts/Queen.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rook : Piece
+public class Queen : Piece
 {
     public override List<Vector2> ListMoves()    //returns an array of the relative locations the piece can move
     {
@@ -45,7 +45,39 @@ public class Rook : Piece
             j++;
             //Debug.Log("Bottom");
         }
-        
+        j = 1;
+        while (position.x + j * tile < 4 && position.y + j * tile < 4 && !PointCollidesWithTeam(new Vector2(tile * j, tile * j) + position, gameObject.tag))
+        {
+            moves.Add(new Vector2(tile * j, tile * j));    //top right
+            if (PointCollidesWithTeam(new Vector2(tile * j, tile * j) + position, OtherTeam(gameObject.tag)))
+                break;
+            j++;
+        }
+        j = 1;
+        while (position.x + j * tile < 4 && position.y - j * tile > -4 && !PointCollidesWithTeam(new Vector2(tile * j, tile * j * -1) + position, gameObject.tag))
+        {
+            moves.Add(new Vector2(tile * j, tile * j * -1));    //bottom right
+            if (PointCollidesWithTeam(new Vector2(tile * j, tile * j * -1) + position, OtherTeam(gameObject.tag)))
+                break;
+            j++;
+        }
+        j = 1;
+        while (position.x - j * tile > -4 && position.y - j * tile > -4 && !PointCollidesWithTeam(new Vector2(tile * j * -1, tile * j * -1) + position, gameObject.tag))
+        {
+            moves.Add(new Vector2(tile * j * -1, tile * j * -1));    //bottom right
+            if (PointCollidesWithTeam(new Vector2(tile * j * -1, tile * j * -1) + position, OtherTeam(gameObject.tag)))
+                break;
+            j++;
+        }
+        j = 1;
+        while (position.x - j * tile > -4 && position.y + j * tile < 4 && !PointCollidesWithTeam(new Vector2(tile * j * -1, tile * j) + position, gameObject.tag))
+        {
+            moves.Add(new Vector2(tile * j * -1, tile * j));    //top right
+            if (PointCollidesWithTeam(new Vector2(tile * j * -1, tile * j) + position, OtherTeam(gameObject.tag)))
+                break;
+            j++;
+        }
+
         for (int i = moves.Count - 1; i >= 0; i--)
         {
             moves[i] += position;
@@ -56,5 +88,5 @@ public class Rook : Piece
 
         return moves;
     }
-    
+
 }

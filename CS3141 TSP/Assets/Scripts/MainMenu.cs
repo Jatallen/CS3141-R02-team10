4573e7939eb5d992f8720078e8fe54d225a6747c
Jatallen <jatallen@mtu.edu>
2022-04-01 11:40:19 -0400

    Added button functionallity
    You can now move between scenes when pressing buttons.

--- CS3141 TSP/Assets/Scripts/MainMenu.cs
+++ CS3141 TSP/Assets/Scripts/MainMenu.cs
@@ -1,24 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
-
+    //Go from main menu to log in page
     public void LogInButton(){
-
+        SceneManager.LoadScene("Log In");
     }
 
+    //Go from login page to chess board
+    public void LogInButton2()
+    {
+        SceneManager.LoadScene("ChessBoard");
+    }
+
+    //Go from main menu to the sign up page
     public void SignUpButton(){
-        
+        SceneManager.LoadScene("Registration");
     }
 
-    // Update is called once per frame
-    void Update(){
-        
+    //Go from the sign up page to the chess board
+    public void SignUpButton2()
+    {
+        SceneManager.LoadScene("ChessBoard");
     }
+
 }

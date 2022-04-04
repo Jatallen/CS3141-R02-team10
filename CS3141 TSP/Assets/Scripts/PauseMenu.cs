34c4798bb69101e3f74987b973a848c755593be1
Chris Apfel <97426721+ChrisA115@users.noreply.github.com>
2022-04-04 03:44:22 -0400

    start of pause menu

---
+++ CS3141 TSP/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject Canvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Escape)){
+
+           if (gameIsPaused){
+               resume();
+           }
+
+           else{
+               pause();
+           }
+
+       }
+
+    }
+
+    void resume(){
+        Canvas.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void pause(){
+        Canvas.SetActive(true);
+        //Time.timeScale = 0f; its chess and tyler will complain so 
+        gameIsPaused = true;
+    }
+
+    public void quiteGame(){
+        Debug.Log("quiting");
+        Application.Quit();
+    }
+}

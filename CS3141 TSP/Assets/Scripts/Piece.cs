8f1228810db820032a04ec51397b9d57296e1fbe
jkyt07 <71299182+jkyt07@users.noreply.github.com>
2022-04-13 11:10:56 -0400

    Only move white
    make it so you can't steal black's moves

--- CS3141 TSP/Assets/Scripts/Piece.cs
+++ CS3141 TSP/Assets/Scripts/Piece.cs
@@ -54,135 +54,144 @@ public abstract class Piece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (GameController.playerSelect == gameObject)  //if the piece is selected
+        if (GameController.turn == "White")
         {
-            if(!pieceSelectedHighlight){
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (GameController.playerSelect == gameObject)  //if the piece is selected
+            {
+                if (!pieceSelectedHighlight)
+                {
                     var instance = Instantiate(highlight, transform.position, Quaternion.identity);
                     Destroy(instance, 1);
 
                     pieceSelectedHighlight = true;
                 }
 
-                if(isHighlighted == false){
+                if (isHighlighted == false)
+                {
 
-                    foreach (Vector2 move in ListMoves()) {
+                    foreach (Vector2 move in ListMoves())
+                    {
                         //Debug.Log(" moves are " + move);
                         var moveInstance = Instantiate(moveHighlight, move, Quaternion.identity);
                         Destroy(moveInstance, 1);
                     }
 
-                isHighlighted = true; 
-                Invoke("setIsHighlightedtoFalse", 1.0f); //update is called constantly, destroys and creates at same rate so when a piece is grabbed is shows
-            }
-
-            transform.position = mousePos;
-            
-            if (Input.GetMouseButtonDown(0))    //left click
-            {
-                if (thisCollider.OverlapPoint(GameController.selectPos))    //if clicked on original spot
-                {
-                    pieceSelectedHighlight = false;
-                    spriteRenderer.sortingOrder = 0;
-                    transform.position = GameController.selectPos;
-                    GameController.playerSelect = null;
-                    //Debug.Log("Put Down: " + position + GameController.selectPos);
+                    isHighlighted = true;
+                    Invoke("setIsHighlightedtoFalse", 1.0f); //update is called constantly, destroys and creates at same rate so when a piece is grabbed is shows
                 }
-                else
+
+                transform.position = mousePos;
+
+                if (Input.GetMouseButtonDown(0))    //left click
                 {
-                    foreach (Vector2 move in ListMoves())   //check if the clicked spot is a possible move
+                    if (thisCollider.OverlapPoint(GameController.selectPos))    //if clicked on original spot
                     {
-                        if (thisCollider.OverlapPoint(move))
-                        {
-                            GameObject pieceAtMove = PieceAt(move);
-                            if (pieceAtMove != null)   //take pieces
-                            {
-                                pieceAtMove.GetComponent<Piece>().TakePiece(gameObject);
-                                //Debug.Log("Piece Taken");
-                            }
-
-                            transform.position = move;
-                            GameController.playerSelect = null;
-                            GameController.turn = OtherTeam(GameController.turn);
-                            
-                            if ((GetComponent<Pawn>() != null))
-                            {
-                                // If pawn reaches back rank, destroy pawn and create a queen in its place
-                                if (Mathf.Abs(transform.position.y) == 3.5)
-                                {
-                                    Destroy(gameObject);
-                                    if (gameObject.tag == "White")
-                                    {
-                                        Instantiate(Queen, transform.position, transform.rotation);
-                                    }
-                                    else
-                                    {
-                                        Instantiate(Queen, transform.position, transform.rotation);
-                                    }
-                                }
-                                if (Mathf.Abs(position.y - move.y) == 2)
-                                    GetComponent<Pawn>().enPassant = true;
-                                else
-                                    GetComponent<Pawn>().enPassant = false;
-
-                                if (position.x != move.x)
-                                {
-                                    GameObject pass = PieceAt(new Vector2(move.x, position.y));
-                                    if (pass != null && pass.GetComponent<Pawn>() != null && pass.GetComponent<Pawn>().enPassant)
-                                        pass.GetComponent<Piece>().TakePiece(gameObject);
-                                }
-
-                            }
-
-                            NoPassant();
-
-
-                            // If castle move by either King is made, move appropriate Rook
-                            if ((GetComponent<King>() != null)){
-                                //Debug.Log("King position: " + position);
-                                if (move.x == -1.5 && position.x == 0.5){
-                                    Destroy(PieceAt(new Vector2((float)-3.5, (float)position.y)));
-                                    Vector2 rookPos = new Vector2(move.x+1,move.y);
-                                    Instantiate(Rook, rookPos, transform.rotation);
-                                }
-                                else if (move.x == 2.5 && position.x == 0.5){
-                                    Destroy(PieceAt(new Vector2((float)3.5, (float)position.y)));
-                                    Vector2 rookPos = new Vector2(move.x-1,move.y);
-                                    Instantiate(Rook, rookPos, transform.rotation);
-                                }
-                            }
-                            //Debug.Log("Successful Move: " + mousePos + move);
-                            hasMoved = true;
-                            isHighlighted = false;
-                            pieceSelectedHighlight = false;
-                            spriteRenderer.sortingOrder = 0;
-                            audioSource.Play();
-                            break;
-                        }
-                        //Debug.Log("Failed Move: " + mousePos + move);
+                        pieceSelectedHighlight = false;
+                        spriteRenderer.sortingOrder = 0;
+                        transform.position = GameController.selectPos;
+                        GameController.playerSelect = null;
+                        //Debug.Log("Put Down: " + position + GameController.selectPos);
                     }
-                    if (ListMoves().Count == 0)
-                        Debug.Log("No Moves");
+                    else
+                    {
+                        foreach (Vector2 move in ListMoves())   //check if the clicked spot is a possible move
+                        {
+                            if (thisCollider.OverlapPoint(move))
+                            {
+                                GameObject pieceAtMove = PieceAt(move);
+                                if (pieceAtMove != null)   //take pieces
+                                {
+                                    pieceAtMove.GetComponent<Piece>().TakePiece(gameObject);
+                                    //Debug.Log("Piece Taken");
+                                }
+
+                                transform.position = move;
+                                GameController.playerSelect = null;
+                                GameController.turn = OtherTeam(GameController.turn);
+
+                                if ((GetComponent<Pawn>() != null))
+                                {
+                                    // If pawn reaches back rank, destroy pawn and create a queen in its place
+                                    if (Mathf.Abs(transform.position.y) == 3.5)
+                                    {
+                                        Destroy(gameObject);
+                                        if (gameObject.tag == "White")
+                                        {
+                                            Instantiate(Queen, transform.position, transform.rotation);
+                                        }
+                                        else
+                                        {
+                                            Instantiate(Queen, transform.position, transform.rotation);
+                                        }
+                                    }
+                                    if (Mathf.Abs(position.y - move.y) == 2)
+                                        GetComponent<Pawn>().enPassant = true;
+                                    else
+                                        GetComponent<Pawn>().enPassant = false;
+
+                                    if (position.x != move.x)
+                                    {
+                                        GameObject pass = PieceAt(new Vector2(move.x, position.y));
+                                        if (pass != null && pass.GetComponent<Pawn>() != null && pass.GetComponent<Pawn>().enPassant)
+                                            pass.GetComponent<Piece>().TakePiece(gameObject);
+                                    }
+
+                                }
+
+                                NoPassant();
+
+
+                                // If castle move by either King is made, move appropriate Rook
+                                if ((GetComponent<King>() != null))
+                                {
+                                    //Debug.Log("King position: " + position);
+                                    if (move.x == -1.5 && position.x == 0.5)
+                                    {
+                                        Destroy(PieceAt(new Vector2((float)-3.5, (float)position.y)));
+                                        Vector2 rookPos = new Vector2(move.x + 1, move.y);
+                                        Instantiate(Rook, rookPos, transform.rotation);
+                                    }
+                                    else if (move.x == 2.5 && position.x == 0.5)
+                                    {
+                                        Destroy(PieceAt(new Vector2((float)3.5, (float)position.y)));
+                                        Vector2 rookPos = new Vector2(move.x - 1, move.y);
+                                        Instantiate(Rook, rookPos, transform.rotation);
+                                    }
+                                }
+                                //Debug.Log("Successful Move: " + mousePos + move);
+                                hasMoved = true;
+                                isHighlighted = false;
+                                pieceSelectedHighlight = false;
+                                spriteRenderer.sortingOrder = 0;
+                                audioSource.Play();
+                                break;
+                            }
+                            //Debug.Log("Failed Move: " + mousePos + move);
+                        }
+                        if (ListMoves().Count == 0)
+                            Debug.Log("No Moves");
+                    }
+                }
+
+            }
+            else
+            {
+                position = new Vector2(transform.position.x, transform.position.y);
+
+                if (Input.GetMouseButtonDown(0) && thisCollider.OverlapPoint(mousePos) && GameController.turn == gameObject.tag && GameController.playerSelect == null) //select this piece
+                {
+                    GameController.playerSelect = gameObject;
+                    GameController.selectPos = position;
+                    spriteRenderer.sortingOrder = 1; //Makes sprite go to front
+
+
+                    //Debug.Log("Pick Up: " + GameController.selectPos);
                 }
             }
-            
+            //Debug.Log(mousePos);
         }
-        else
-        {
-            position = new Vector2(transform.position.x, transform.position.y);
-
-            if (Input.GetMouseButtonDown(0) && thisCollider.OverlapPoint(mousePos) && GameController.turn == gameObject.tag && GameController.playerSelect == null) //select this piece
-            {
-                GameController.playerSelect = gameObject;
-                GameController.selectPos = position;
-                spriteRenderer.sortingOrder = 1; //Makes sprite go to front
-                
-
-                //Debug.Log("Pick Up: " + GameController.selectPos);
-            }
-        }
-        //Debug.Log(mousePos);
     }
 
     //return whether or not the point collides with a piece of the given team

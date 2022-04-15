04b56876812f6186e2750751f5f69c6118299789
Jatallen <jatallen@mtu.edu>
2022-04-15 11:43:50 -0400

    Made Sign up and Login functional

--- CS3141 TSP/Assets/Scripts/MainMenu.cs
+++ CS3141 TSP/Assets/Scripts/MainMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using System.IO;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
@@ -11,26 +12,6 @@ public class MainMenu : MonoBehaviour
     private string input;
     public TMP_InputField username;
     public TMP_InputField password;
-
-
-    void Security()
-    {
-      /*  string path = Application.dataPath + "/Log-in.txt";
-        Debug.Log(path);
-        if (!File.Exists(path))
-        {
-            File.WriteAllText(path, "Log-in \n\n");
-        }
-        string test = "test\n";
-        File.AppendAllText(path, test);
-       */    
-     }
-
-    void Start()
-    {
-        Security();
-
-    }
 
     //Go from main menu to log in page
     public void LogInButton(){
@@ -40,10 +21,19 @@ public class MainMenu : MonoBehaviour
     //Go from login page to chess board
     public void LogInButton2()
     {
-        //Debug.Log("please " + username.text);
+        string path = Application.dataPath + "/Log-in.txt";
+        Debug.Log(path);
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "Log-in \n\n");
+        }
+        string[] vals = File.ReadAllLines(path);
+        int exists = Array.IndexOf(vals, username.text);
 
-
-        SceneManager.LoadScene("PlayScreen");
+        if( vals[exists + 1].Equals(password.text) )
+        {
+            SceneManager.LoadScene("PlayScreen");
+        }
     }
 
     //Go from main menu to the sign up page
@@ -60,9 +50,15 @@ public class MainMenu : MonoBehaviour
         {
             File.WriteAllText(path, "Log-in \n\n");
         }
-        string userInfo = username.text + "    " + password.text + "\n";
-        File.AppendAllText(path, userInfo);
-        SceneManager.LoadScene("PlayScreen");
+        string userInfo = username.text + "\n" + password.text + "\n";
+        string[] vals = File.ReadAllLines(path);
+        int exists = Array.IndexOf(vals, username.text);
+
+        if (!(exists > 0))
+        {
+            File.AppendAllText(path, userInfo);
+            SceneManager.LoadScene("PlayScreen");
+        }
     }
 
     public void PlayButton()
